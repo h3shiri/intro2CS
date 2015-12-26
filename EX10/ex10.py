83e62d82a2fb79e321cc478de810a81c153990f1
S.H <h3shiri@gmail.com>
2015-12-27 01:27:04 +0200

    EX10

---
+++ EX10/ex10.py
@@ -0,0 +1,14 @@
+
+# reading the links from a given file.
+def read_article_links(file_name):
+    res = []
+    file = open(file_name, 'r')
+    text = file.read()
+    links = text.split('\t')
+    # appending all the links one by one
+    while len(links) > 0:
+        node1 = links.pop(0)
+        node2 = links.pop(0)
+        edge = (node1, node2)
+        res.append(edge)
+    return res
