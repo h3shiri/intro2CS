696d0e1c78b64b8cdfcc33276a93755f1da28242
S.H <h3shiri@gmail.com>
2015-12-27 02:07:43 +0200

    plaing with sublime

--- EX10/ex10.py
+++ EX10/ex10.py
@@ -6,10 +6,9 @@ def read_article_links(file_name):
     text = file.read()
     links = text.split('\t')
     # appending all the links one by one
-    while len(links) > 0:
+    while len(links) >= 2:
         node1 = links.pop(0)
         node2 = links.pop(0)
         edge = (node1, node2)
         res.append(edge)
     return res
-    
