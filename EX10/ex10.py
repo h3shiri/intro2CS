34f2e6f97c1b13fc536bae1dc74947afac83810a
S.H <h3shiri@gmail.com>
2015-12-27 23:13:23 +0200

    Moshe shows off his skills

--- EX10/ex10.py
+++ EX10/ex10.py
@@ -44,3 +44,77 @@ class Article:
     def __contains__(self, article):
         res = article in self.collection
         return res
+
+class WikiNetwork:
+
+    def __init__(self, link_list = []):
+        self.__network = dict()
+        self.update_network(link_list)
+        
+    def update_network(self, link_list = []):
+        for link in link_list:
+            source = link[0]
+            target = link[1]
+            if source not in self.__network.keys():
+                self.__network[source] = Article(source)
+
+            if target not in self.__network.keys():
+                self.__network[target] = Article(target)
+
+            self.__network[source].add_neighbor(self.__network[target])
+
+    def get_articles(self):
+        return self.__network.values()
+
+    def get_titles(self):
+        return self.__network.keys()
+
+    def __contains__(self, article_name):
+        return (article_name in self.get_titles())
+
+    def __len__(self, article_name):
+        return self.__network.len()
+
+    def __repr__(self):
+        return str(self.__network)
+
+    def __getitem__(self, article_name):
+        if article_name not in self.get_titles():
+            raise KeyError(article_name)
+        return self.__network[article_name]
+
+    def page_rank(self, iters, d=0.9):
+        # We create a dictionary that contains, for each title, it's page rank number
+        ranks = dict()
+        
+        # Create all titles in dictionary and set their ranks to 1
+        for title in self.get_titles():
+            ranks[title] = 1
+
+        # Here are the iterations
+        for i in range(iters):
+
+            # Create the new ranks dictionary. We want this to be separate so that by changing the ranks
+            # we don't affect the old values.
+            newRanks = dict()
+            for title in self.get_titles():
+                newRanks[title] = 1 - d
+
+            # Update the page ranks
+            for title, rank in ranks.items():
+                for neighbor in self[title].get_neighbors():
+                    newRanks[neighbor.get_name()] += ranks[title]/len(self[title].get_neighbors())
+
+            ranks = copy.deepcopy(newRanks)
+
+        sortedByTitle = sorted(ranks.items(), key=lambda a: a[0])
+        sortedByRank = sorted(sortedByTitle, key=lambda a: a[1], reverse=True)
+
+        # print(sortedByRank)
+
+        return [ titleAndRankTuple[0] for titleAndRankTuple in sortedByRank ]
+
+#TODO: remove tests later on
+network = WikiNetwork(read_article_links('links.txt'));
+print('Hello!')
+print(network.page_rank(3))
