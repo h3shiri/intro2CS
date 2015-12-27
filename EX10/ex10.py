e3e92641339f0c0ef3e9420ff39a25aae28e4f19
S.H <h3shiri@gmail.com>
2015-12-27 02:32:34 +0200

    made part of the class

--- EX10/ex10.py
+++ EX10/ex10.py
@@ -1,4 +1,4 @@
-
+import copy
 # reading the links from a given file.
 def read_article_links(file_name):
     res = []
@@ -12,3 +12,35 @@ def read_article_links(file_name):
         edge = (node1, node2)
         res.append(edge)
     return res
+
+class Article:
+    """docstring for Article"""
+    def __init__(self, name):
+        self.__name = name
+        #figured out this collections
+        self.collection = []
+
+    # Retrurn the name of the article
+    def get_name(self):
+        return self.__name
+    # Add a neighbor to the collection
+    def add_neighbor(self, neighbor):
+        self.collection.append(neighbor)
+    # Return the collection list (neighbors)
+    def get_neighbors(self):
+        res = copy.deepcopy(self.collection)
+        return res
+    # Represent an Article format
+    def __repr__(self):
+        r1 = self.get_name()
+        r2 = self.get_neighbors()
+        res = str(r1, r2)
+        print(res)
+    # Return the number of neighbors
+    def __len__(self):
+        res = len(self.get_neighbors())
+        return res
+    # Determine whether an article is an outgoing neighbor
+    def __contains__(self, article):
+        res = article in self.collection
+        return res
