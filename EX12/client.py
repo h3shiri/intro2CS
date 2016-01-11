3365ae9b40a62a560f697f73900f463488fbc1a9
S.H <h3shiri@gmail.com>
2016-01-11 23:01:42 +0200

    Book at least 10h for this..etc

--- EX12/client.py
+++ EX12/client.py
@@ -1,24 +1,31 @@
+import sys
 import socket
 import math
 import PIL
 import copy
 
+class Message:
+	def __init__(self):
+		self.__send_message = ""
+
+
 # Receiving data from server...etc
 HOST = socket.gethostname()
-PORT = 6789
+PORT = 5679
 MAXIMUM_NUMBER_OF_QUEUED_CONNECTIONS = 0
 MAX_DATA_CHUNK = 1024
-
+print("Before the sockets")
 server_socket = socket.socket()
 server_socket.bind((HOST, PORT))
 server_socket.listen(MAXIMUM_NUMBER_OF_QUEUED_CONNECTIONS)
 
 (transmission_socket, client_address) = server_socket.accept()
-
+print("pre-transmition")
 while True:
     encoded_data = transmission_socket.recv(MAX_DATA_CHUNK)
     if not encoded_data:
         # Data transmission ended
+        print("Hello!")
         break
     else:
         # Do something with the data
