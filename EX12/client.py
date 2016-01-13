ad4dae959ce13fc0cf0d14afe204d015fd796411
Shiri Heffetz <h3shiri@gmail.com>
2016-01-13 21:47:33 +0200

    "final version v1 let's go the Gym!!"

--- EX12/client.py
+++ EX12/client.py
@@ -1,8 +1,5 @@
 import sys
 import socket
-import math
-import PIL
-import copy
 import Gui
 import tkinter as tki
 import select
@@ -14,8 +11,12 @@ MAX_DATA_CHUNK = 1024
 MSG_DELIMETER = b'\n'
     
 class Client():
-    """docstring for Client"""
-    def __init__(self, username,group):
+    """
+    This class represents the client interface to the server,
+    we also handle the server messages from her and connect to the gui.
+    """
+    # Initialise the class and set all basic parameters
+    def __init__(self, username, group):
         self._online_users = set()
         self.username = username
         self._online_users.add(username)
@@ -23,7 +24,7 @@ class Client():
         self.connect_to_server()
         Ultimate_root = tki.Tk()
         self.gui = Gui.GuiRunner(Ultimate_root,self)
-        self.gui.AddToUserBox()
+        self.gui.add_to_user_box()
         self.gui.queue_for_running(self.recieve_server_messages)
         Ultimate_root.mainloop()
 
@@ -31,11 +32,17 @@ class Client():
         self.socket.sendall(msg.encode())
 
     def connect_to_server(self):
+        """ this function makes socket connect to server
+        """
         self.socket = socket.socket()
         self.socket.connect((HOST,PORT))
         self.__send_message__('join;%s;%s\n'%(username,group))
     
     def recieve_server_messages(self):
+        """ this functions receives a message from server.
+        It decodes it according to server protocol message,
+        It sends the message to Gui.
+        """
         while True:
             socket = self.socket
             read_list,w,x = select.select([socket], [], [], 0.01)
@@ -46,15 +53,16 @@ class Client():
                     message = data.decode()
                     self._message_handler = Message(message)
                     self.event_handler(self._message_handler)
-                    #TODO: USe the message accordingly..
-                    print("serverMessage:%s"%(message))
             break
         self.gui.queue_for_running(self.recieve_server_messages)
 
     def event_handler(self, message_class):
-        print(message_class._data_list)
-        print(message_class.actions_types)
-        while((len(message_class._data_list) > 0) and (len(message_class.actions_types)>0)):
+        """ This function gets a message and makes an output according to
+        given information
+
+        """
+        while((len(message_class._data_list) > 0) and
+                  (len(message_class.actions_types)>0)):
             action = (message_class.actions_types.pop(0))
             data = (message_class._data_list.pop(0))
             # join,leave or Users actions.
@@ -62,17 +70,17 @@ class Client():
                 users = data[1].split(",")
                 for user in users:
                     self._online_users.add(user)
-                    self.gui.AddToUserBox()
+                    self.gui.add_to_user_box()
             
             elif action == "join":
                 user = data[1]
                 self._online_users.add(user)
-                self.gui.AddToUserBox()
+                self.gui.add_to_user_box()
             elif action == 'leave':
                 user = data[1]
                 if user in self._online_users:
                     self._online_users.remove(user)
-                    self.gui.deleteUserFromUserBox(user)
+                    self.gui.delete_user_from_user_box(user)
             elif action == "error":
                 error = data[1]
                 self.gui._debug_message = error
@@ -80,23 +88,28 @@ class Client():
                 self.shape_proccesor(data)
     
     def shape_proccesor(self, data):
-
+        """ This function gets data and makes Gui draw given information.
+        Delivers username as well.
+        """
         username = data[1]
         shape = data[2]
         color = data[-1]
         coordinates = data[3].split(",")
         if shape == 'line' and data[1] != self.username:
-            self.gui.createLine(coordinates, color, username)
+            self.gui.create_line(coordinates, color, username)
         elif shape == 'rectangle' and data[1] != self.username:
-            self.gui.createRectangle(coordinates, color, username)
+            self.gui.create_rectangle(coordinates, color, username)
         elif shape == 'oval' and data[1] != self.username:
-            self.gui.createCircle(coordinates, color, username)
+            self.gui.create_circle(coordinates, color, username)
         elif shape == 'triangle' and data[1] != self.username:
-            self.gui.createTriangle(coordinates, color, username)
+            self.gui.create_triangle(coordinates, color, username)
 
 
 class Message():
-    """docstring for Message"""
+    """This class delivers messages and receives messages from server
+    according to format
+    Make sure that client serener are communicating"""
+    #Initialise the class and set all basic parameters
     def __init__(self, Server_message):
         self.__server_message = Server_message
         self.actions_types = ["cookie"]
@@ -104,13 +117,16 @@ class Message():
         self.decipher()
 
     def decipher(self):
+        """ reformats server message to useful parameters inside the class
+
+        """
         messages = self.__server_message.split("\n")
         num_of_messages = len(messages)
-
         for message in messages:
             if len(message) != 0:
                 new_message_parameters = message.split(";")
-                new_message_parameters[-1] = new_message_parameters[-1].strip("\n")
+                new_message_parameters[-1] = \
+                    new_message_parameters[-1].strip("\n")
                 if self.actions_types[0] == "cookie":
                     self.actions_types[0] = new_message_parameters[0]
                     self._data_list[0] = new_message_parameters
@@ -125,5 +141,3 @@ if __name__ == '__main__':
     group = sys.argv[4]
     Client(username, group)
 
-#TODO: solve board filling/update problem when joinnning an already existing game..)
-#TODO: remember to remove all the prints orders
